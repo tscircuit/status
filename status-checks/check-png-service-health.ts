550b3810c8281c517d9915d03ec8ed5eb033c195
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-05-12 22:23:59 +0530

    fix: increase the wait timeout for the png_service check (#43)
    * fix: increase the wait timeout for the png_service check
    
    * formatbot: Automatically format code
    
    ---------
    
    Co-authored-by: tscircuitbot <tscircuitbot@users.noreply.github.com>

--- status-checks/check-png-service-health.ts
+++ status-checks/check-png-service-health.ts
@@ -7,7 +7,7 @@ export const checkPngServiceHealth: HealthCheckFunction = async () => {
     const url = `https://png.tscircuit.com/?view=3d&code=H4sIAJsBqGcAA42QsQ6CMBRFdxP%2F4aUTDEJBNikLiR%2FA5FpKkUahTanRxPjvVksb4uTWc3JfTlL%2BUFIb6HhPb1cDUQykgmi7AShbSXUHd9GZgaAMjyOCgYvzYBaqPiu703wWs5HaIYBjOjFOUH5BXvdSGqXFZM9xgfPgJzraYZMFMbPhRJ7Fy7NireX9wqnPMqooE6vuIly4wBir45%2Fx%2Bje%2B8zVfDyLkjaaMQ6%2FlSFDSZFBBosSUITDSinol3EmZfv%2FTvuPDG02y%2Bwx1AQAA&cachebust=${randomParam}`
 
     await ky.get(url, {
-      timeout: 5000,
+      timeout: 30000,
     })
 
     return { ok: true }
